6e3e5e90473b12e1ccbac9bb2a94a0a7c1b3d57e
BiryukovSergey <twc2012@yandex.ru>
2021-08-30 16:43:08 +0300

    Controller
    Накидал движение и прыжок

---
+++ Hot Ball/Assets/Code/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+namespace Code
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        [SerializeField]private float _speedPlayer;
+        [SerializeField]private Transform _transformPlayer;
+        private float _deltaTimeSpeed;
+        private Vector3 _movePlayer;
+        private PlayerJump _jumpPlayer;
+        private PlayerMove _playerMove;
+
+        public PlayerMove(Transform transform, float speed)
+        {
+            _speedPlayer = speed;
+            _transformPlayer = transform;
+        }
+        private void Awake()
+        {
+            _playerMove = new PlayerMove(_transformPlayer, _speedPlayer);
+        }
+
+        private void Update()
+        {
+            _playerMove._movePlayer.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            _deltaTimeSpeed = Time.deltaTime * _speedPlayer;
+            _playerMove._transformPlayer.Translate(  _playerMove._movePlayer * _deltaTimeSpeed, Space.World);
+            transform.localPosition += _movePlayer;
+        }
+    }
+}
---
+++ Hot Ball/Assets/Code/Vector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+    public struct Vector
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Z { get; private set; }
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public static explicit operator Vector(double x) => new Vector(x,x,x);
+        public static implicit operator double(Vector x) => x.X;
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            Vector res = new Vector();
+            {
+                res.X = v1.X + v2.X;
+                res.Y = v1.Y + v2.Y;
+                res.Z = v1.Z + v2.Z;
+            }
+            return res;
+        }
+    }
+}
