088f4ec2cbf8528a5a6eb4c47540218deedc1d8a
BiryukovSergey <twc2012@yandex.ru>
2021-10-03 14:34:30 +0300

    Шар едет прямо и в сторону - одновременно

--- Hot Ball/Assets/Code/Player/PlayerMove.cs
+++ Hot Ball/Assets/Code/Player/PlayerMove.cs
@@ -5,8 +5,7 @@ public class PlayerMove
         private Vector3 _move;
         public void Move(float speed, Rigidbody rigidbody)
         {
-            _move += new Vector3(Input.GetAxis(InputManager.HORIZONTAL), 0, (Vector3.forward.z * speed *Time.deltaTime)); 
-            // <- едет только в сторону, но не прямо и в сторону
+            _move += new Vector3(Input.GetAxis(InputManager.HORIZONTAL), 0, (Vector3.forward.z * speed)); 
             _move = Vector3.ClampMagnitude(_move, speed);
             _move.y = rigidbody.velocity.y;
             rigidbody.velocity = _move;
